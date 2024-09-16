40e6168d190cf2853c1207fd1d3f94619643950b
Sophia <wwan18@my.bcit.ca>
2024-09-16 11:01:04 -0700

    finalwithsignature

--- practice.js
+++ practice.js
@@ -9,7 +9,7 @@ let spare = "food";
 
 console.log("During my free time I like to play " + hobbies + "." + " Some " + hobbies + " " + "I like to play are League, Valorant, and Roblox. Usually I like to play games with my friends on the weekend since all of us are busy with school and our schedules do not line up. Personally I do not playing " + hobbies + " like Valorant, but my friends enjoy playing it so I play with them.When I am not playing" + " " + hobbies + " "+ "I like to eat a lot of " + spare + " " + "specifically snacks."); 
 
-//comment: can seperate paragraph into two console.log so when it comes to editing, it will be easier.
+//comment: can seperate paragraph into two console.log so when it comes to editing, it will be easier. - Sandy
 
 spare = "nap";
 
@@ -18,4 +18,4 @@ console.log("\x1b[36m","If I am not eating, I like to" + " " + spare +"! When I 
 spare = "napping"
 
 console.log("But I end up " + spare + " for over " + 3 + " hours at times.", "\x1b[0m");
-// I love sleeping too! 
+// I love sleeping too!  - Sandy
