a019fed339d999f6bf9ee139b101f9a1e532e55f
Sophia <wwan18@my.bcit.ca>
2024-09-16 11:00:17 -0700

    finaledit

--- practice.js
+++ practice.js
@@ -2,16 +2,20 @@
 // step 1 - const hobbies = games let spare= food
 // step 2- console.log("")
 //step 3- topic is hobbies games- league roblox val,food,sleeping
+// write about what games i like to play, what i do if im not playing games such as eating and sleeping,who i play games with.
 
 const hobbies = "games";
 let spare = "food";
 
-console.log("During my free time I like to play " + hobbies + "." + " Some " + hobbies + " " + "I like to play are League, Valorant, and Roblox. Usually I like to play games with my friends on the weekend since all of us are busy with school and our schedules do not line up. Personally I do not playing " + hobbies + " like Valorant, but my friends enjoy playing it so I play with them.When I am not playing" + " " + hobbies + " "+ "I like to eat alot of " + spare + " " + "specifically snacks.");
+console.log("During my free time I like to play " + hobbies + "." + " Some " + hobbies + " " + "I like to play are League, Valorant, and Roblox. Usually I like to play games with my friends on the weekend since all of us are busy with school and our schedules do not line up. Personally I do not playing " + hobbies + " like Valorant, but my friends enjoy playing it so I play with them.When I am not playing" + " " + hobbies + " "+ "I like to eat a lot of " + spare + " " + "specifically snacks."); 
+
+//comment: can seperate paragraph into two console.log so when it comes to editing, it will be easier.
 
 spare = "nap";
 
-console.log("If I am not eating, I like to" + " " + spare +"! When I " + spare + " I try to " + spare + " for atleast " + 30 + " minutes.");
+console.log("\x1b[36m","If I am not eating, I like to" + " " + spare +"! When I " + spare + " I try to " + spare + " for atleast " + 30 + " minutes.");
 
 spare = "napping"
 
-console.log("\x1b[32m","But I end up " + spare + " for over " + 3 + " hours at times.");
+console.log("But I end up " + spare + " for over " + 3 + " hours at times.", "\x1b[0m");
+// I love sleeping too! 
