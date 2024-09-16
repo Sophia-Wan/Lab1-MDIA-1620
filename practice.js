5ca9641b801172640d1a91a734c9a27e2c22a7fa
Sophia <wwan18@my.bcit.ca>
2024-09-16 10:37:05 -0700

    spacings

--- practice.js
+++ practice.js
@@ -2,10 +2,16 @@
 // step 1 - const hobbies = games let spare= food
 // step 2- console.log("")
 //step 3- topic is hobbies games- league roblox val,food,sleeping
+
 const hobbies = "games";
 let spare = "food";
-console.log("During my free time I like to play" + " " + hobbies + "." + " Some" + " " + hobbies + " " + "I like to play are League, Valorant, and Roblox. Usually I like to play games with my friends on the weekend since all of us are busy with school and our schedules do not line up. When I am not playing" + " " + hobbies + " "+ "I like to eat alot of " + spare + " " + "specifically snacks.");
+
+console.log("During my free time I like to play " + hobbies + "." + " Some " + hobbies + " " + "I like to play are League, Valorant, and Roblox. Usually I like to play games with my friends on the weekend since all of us are busy with school and our schedules do not line up. Personally I do not playing " + hobbies + " like Valorant, but my friends enjoy playing it so I play with them.When I am not playing" + " " + hobbies + " "+ "I like to eat alot of " + spare + " " + "specifically snacks.");
+
 spare = "nap";
+
 console.log("If I am not eating, I like to" + " " + spare +"! When I " + spare + " I try to " + spare + " for atleast " + 30 + " minutes.");
+
 spare = "napping"
+
 console.log("\x1b[36m","But I end up " + spare + " for over " + 3 + " hours at times.")
