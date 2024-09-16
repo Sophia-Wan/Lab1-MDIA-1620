d96ad097f9608d84d850cf0ac7b9acbb44479fa4
Sophia <wwan18@my.bcit.ca>
2024-09-16 10:30:51 -0700

    edited2

--- practice.js
+++ practice.js
@@ -4,8 +4,8 @@
 //step 3- topic is hobbies games- league roblox val,food,sleeping
 const hobbies = "games";
 let spare = "food";
-console.log("During my free time I like to play" + " " + hobbies + "." + " Some" + " " + hobbies + " " + "I like to play are League, Valorant, and Roblox. When I am not playing" + " " + hobbies + " "+ "I like to eat alot of " + spare + " " + "specifically snacks.");
+console.log("During my free time I like to play" + " " + hobbies + "." + " Some" + " " + hobbies + " " + "I like to play are League, Valorant, and Roblox. Usually I like to play games with my friends on the weekend since all of us are busy with school and our schedules do not line up. When I am not playing" + " " + hobbies + " "+ "I like to eat alot of " + spare + " " + "specifically snacks.");
 spare = "nap";
 console.log("If I am not eating, I like to" + " " + spare +"! When I " + spare + " I try to " + spare + " for atleast " + 30 + " minutes.");
 spare = "napping"
-console.log("But I end up " + spare + " for over " + 3 + " hours at times.")
+console.log("\x1b[36m","But I end up " + spare + " for over " + 3 + " hours at times.")
